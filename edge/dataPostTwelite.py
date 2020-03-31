a4863835058c89d44417946b6467b8e9476b4df3
uribo <ur1b0.dev@gmail.com>
2020-03-31 15:11:36 +0900

    生データをそのまま送信するよう変更

--- edge/dataPostTwelite.py
+++ edge/dataPostTwelite.py
@@ -10,5 +10,4 @@ while True:
     line = ser.readline().decode("utf-8")
     line = "\'" + line.rstrip() + "\'"
     time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#    print(line)
     os.system("curl -H 'Content-Type:application/json' -d " + line + " http://" + param_edge.ADDRESS + ":" + "8048" +" -so -X POST " )

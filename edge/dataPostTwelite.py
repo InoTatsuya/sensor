3d6696f5a476ff83ca8d4c72bb498799f634ce72
uribo <ur1b0.dev@gmail.com>
2020-05-22 22:25:15 +0900

    osモジュールからsubprocessに変更

--- edge/dataPostTwelite.py
+++ edge/dataPostTwelite.py
@@ -1,13 +1,20 @@
 import serial
 import datetime
-import os
+import subprocess as sp
 import param_edge
 
 # シリアル接続
 ser = serial.Serial(param_edge.SERIAL_TWE,115200)
 
 while True:
-    time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    line = ser.readline().decode("utf-8")
-    line = "\'" + time + "," + line.rstrip() + "\'"
-    os.system("curl -H 'Content-Type:application/json' -d " + line + " http://" + param_edge.ADDRESS + ":" + "8048" +" -so -X POST " )
+    line = ser.readline().decode("utf-8").replace("\0","").replace("\n","")
+    print(line.encode())
+    if line:
+        time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        line = "\'" + time + "," + line.rstrip() + "\'"
+        line = "curl -H 'Content-Type:application/json' -d " + line + " http://" + param_edge.ADDRESS + ":" + "8049" +" -so -X POST "
+        print(line.encode())
+        sp.Popen(line, shell=True)
+#        print("send!")
+    else:
+        print( "line is b'0x00'!!!" )
